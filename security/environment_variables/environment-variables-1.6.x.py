2e5e86778a72fe2838d3bc8ace36ddb99c8b4079
Archimedes-osei <66987017+Archimedes-osei@users.noreply.github.com>
2021-07-15 17:37:57 +0000

    Update environment-variables-1.6.x.py

--- security/environment_variables/environment-variables-1.6.x.py
+++ security/environment_variables/environment-variables-1.6.x.py
@@ -5,8 +5,8 @@ from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/user/account
 # To set up environmental variables, see http://twil.io/secure
-account_sid = os.environ['TWILIO_ACCOUNT_SID']
-auth_token = os.environ['TWILIO_AUTH_TOKEN']
+account_sid = os.environ['AC534dccd7e2f3b001233999ce0072b577']
+auth_token = os.environ['63574844890836de8ee1a4545a18c152']
 client = Client(account_sid, auth_token)
 
 # Make API calls here...
